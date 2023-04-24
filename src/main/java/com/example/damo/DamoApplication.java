aa372beee82295098e897e631aa3851ac69afcdd
hoonloper <yonghoonj98@gmail.com>
2023-04-24 20:49:45 +0900

    Setting: 초기 스프링 세팅

---
+++ src/main/java/com/example/damo/DamoApplication.java
@@ -0,0 +1,13 @@
+package com.example.damo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamoApplication.class, args);
+	}
+
+}
