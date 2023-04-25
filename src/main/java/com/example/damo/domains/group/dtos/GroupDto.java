cffd5e70814b4b1e0e9eb711ad8ba8e1e6e20f70
hoonloper <yonghoonj98@gmail.com>
2023-04-25 19:43:55 +0900

    Feat: println 제거 및 Lombok @Data 어노테이션 사용

--- src/main/java/com/example/damo/domains/group/dtos/GroupDto.java
+++ src/main/java/com/example/damo/domains/group/dtos/GroupDto.java
@@ -1,7 +1,6 @@
 package com.example.damo.domains.group.dtos;
 
 import com.example.damo.domains.group.entities.Group;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -18,21 +17,6 @@ public class GroupDto {
     private LocalDateTime deadlineAt;
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
-
-    @Builder
-    public GroupDto(Long id, Long userId, Integer typeId, String name, String introduction, Integer maxUser, Boolean isDone, LocalDateTime deadlineAt, LocalDateTime createdAt, LocalDateTime deletedAt) {
-        this.id = id;
-        this.userId = userId;
-        this.typeId = typeId;
-        this.name = name;
-        this.introduction = introduction;
-        this.maxUser = maxUser;
-        this.isDone = isDone;
-        this.deadlineAt = deadlineAt;
-        this.createdAt = createdAt;
-        this.deletedAt = deletedAt;
-    }
-
     public Group toEntity(){
         return Group.builder()
                 .id(id)
@@ -47,72 +31,4 @@ public class GroupDto {
                 .deletedAt(deletedAt)
                 .build();
     }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Integer getTypeId() {
-        return typeId;
-    }
-    public void setTypeId(Integer typeId) {
-        this.typeId = typeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIntroduction() {
-        return introduction;
-    }
-    public void setIntroduction(String introduction) {
-        this.introduction = introduction;
-    }
-
-    public Integer getMaxUser() {
-        return maxUser;
-    }
-    public void setMaxUser(Integer maxUser) {
-        this.maxUser = maxUser;
-    }
-
-    public void setDone(Boolean isDone) {
-        this.isDone = isDone;
-    }
-
-    public LocalDateTime getDeadlineAt() {
-        return deadlineAt;
-    }
-    public void setDeadlineAt(LocalDateTime deadlineAt) {
-        this.deadlineAt = deadlineAt;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getDeletedAt() {
-        return deletedAt;
-    }
-    public void setDeletedAt(LocalDateTime deletedAt) {
-        this.deletedAt = deletedAt;
-    }
-
 }
--- src/main/java/com/example/damo/domains/group/services/GroupWriteService.java
+++ src/main/java/com/example/damo/domains/group/services/GroupWriteService.java
@@ -12,17 +12,6 @@ public class GroupWriteService {
     private final GroupRepository groupRepository;
 
     public void create(GroupDto groupDto) {
-        Group group = groupDto.toEntity();
-        System.out.println(group.getId());
-        System.out.println(group.getIntroduction());
-        System.out.println(group.getUserId());
-        System.out.println(group.getTypeId());
-        System.out.println(group.getName());
-        System.out.println(group.getDeadlineAt());
-        System.out.println(group.getDeletedAt());
-        System.out.println(group.getCreatedAt());
-        System.out.println(group.getIsDone());
-        System.out.println(group.getMaxUser());
-        groupRepository.save(group);
+        groupRepository.save(groupDto.toEntity());
     }
 }
